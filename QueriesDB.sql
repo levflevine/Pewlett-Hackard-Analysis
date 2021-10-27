77ffdb79b612bd70584a80cb2584c1b45c8136c5
levflevine <levflevine@gmail.com>
2021-10-27 19:16:13 -0400

    Employees by Department

--- QueriesDB.sql
+++ QueriesDB.sql
@@ -20,8 +20,8 @@ CREATE TABLE dept_manager (
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+    --PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	PRIMARY KEY (title, emp_no, from_date),
+	--PRIMARY KEY (title, emp_no, from_date),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 CREATE TABLE dept_emp (
@@ -44,9 +44,55 @@ CREATE TABLE dept_emp (
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,	
-	PRIMARY KEY (emp_no, dept_no, from_date),
+	--PRIMARY KEY (emp_no, dept_no, from_date),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
---DROP TABLE dept_emp;
-SELECT * FROM dept_emp;
+
+DROP TABLE retirement_info;
+
+SELECT * FROM departments LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+FROM employees as e
+LEFT JOIN dept_emp as de ON e.emp_no = de.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
