b318ad986a9da9f04f45e5a9b9dc1bb66245cfe2
levflevine <levflevine@gmail.com>
2021-10-27 23:10:45 -0400

    Deliverable 1

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,34 @@
+SELECT e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	t.title, 
+	t.from_date, 
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no ASC;
+
+SELECT * FROM retirement_titles;
+
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name, 
+	rt.last_name, 
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no ASC,
+		rt.to_date DESC;
+ 
+SELECT * FROM unique_titles LIMIT 10;
+
+SELECT COUNT(ut.emp_no),
+			ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY 1 DESC;
+
+SELECT * FROM retiring_titles LIMIT 10;
