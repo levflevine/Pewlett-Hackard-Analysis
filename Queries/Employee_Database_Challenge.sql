1d98a5d11bedf5e95356b0e3549b11e03c14b59f
levflevine <levflevine@gmail.com>
2021-10-28 00:04:31 -0400

    Deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -38,15 +38,19 @@ SELECT * FROM retiring_titles LIMIT 10;
 
 -- Deliverable 2 --
 
-SELECT DISTINCT (e.emp_no) e.emp_no,
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	e.first_name, 
 	e.last_name, 
-	t.title, 
-	t.from_date, 
-	t.to_date
---INTO retirement_titles
+	e.birth_date,
+	de.from_date, 
+	de.to_date, 
+	t.title
+INTO mentorship_eligibilty
 FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
 LEFT JOIN titles as t
 ON e.emp_no = t.emp_no
-WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no ASC;
